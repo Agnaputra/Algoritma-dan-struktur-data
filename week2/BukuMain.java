ac6b632980e7a1b04569fe4a652b2a824bea81e2
nanaputra <agenaputra@gmail.com>
2024-02-23 17:07:43 +0700

    Update BukuMain.java

--- week2/BukuMain.java
+++ week2/BukuMain.java
@@ -13,14 +13,26 @@ public class BukuMain {
      bk1.terjual(5);
      bk1.gantiHarga(60000);
      bk1.tampilInformasi();
+     System.out.println("Harga yang terjual + "+bk1.hitungHargaTotal(5));
+     System.out.println("Diskon = "+ bk1.hitungDiskon(5));
+     System.out.println("Bayar ="+ bk1.hitungHargaBayar(5));
+     System.out.println("=======================================");
 
     buku01 bk2 = new buku01("Self Reward", "Maheera Ayesha", 160, 29, 59000);
     bk2.terjual(11);
     bk2.tampilInformasi();
+    System.out.println("Harga yang terjual + "+bk2.hitungHargaTotal(11));
+    System.out.println("Diskon = "+ bk2.hitungDiskon(11));
+    System.out.println("Bayar ="+ bk2.hitungHargaBayar(11));
+    System.out.println("=======================================");
 
     buku01 bukuAgna = new buku01("The Art of Problem Solving", "Denanda Pratiwi", 300, 20, 120000);
     bukuAgna.terjual(10);
     bukuAgna.tampilInformasi();
+    System.out.println("Harga yang terjual + "+bukuAgna.hitungHargaTotal(10));
+    System.out.println("Diskon = "+ bukuAgna.hitungDiskon(10));
+    System.out.println("Bayar ="+ bukuAgna.hitungHargaBayar(10));
+    System.out.println("=======================================");
     }
 
 }
