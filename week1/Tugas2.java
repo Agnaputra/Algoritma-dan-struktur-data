f0cf76d1051490fef8779f99d2f712cf68753f88
nanaputra <agenaputra@gmail.com>
2024-02-23 13:52:58 +0700

    Update Tugas2.java

--- week1/Tugas2.java
+++ week1/Tugas2.java
@@ -1,29 +1,29 @@
 package week1;
 import java.util.Scanner;
 
-public class Tugas2 {
-
+public class Tugas2{
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        
-        System.out.print("Masukkan NIM: ");
-        String nim = input.nextLine();
-        
-        int n = Integer.parseInt(nim.substring(nim.length()-2));
-        
-        if(n < 10) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input NIM: ");
+        String nim = scanner.nextLine();
+        System.out.print("OUTPUT : ");
+        printSeries(nim);
+    }
+
+    public static void printSeries(String nim) {
+        int n = Integer.parseInt(nim.substring(nim.length() - 2));
+        if (n < 10) {
             n += 10;
         }
-        
-        for(int i = 1; i <= n; i++) {
-            if(i == 6 || i == 10) {
-                continue; 
+
+        for (int i = 1; i <= n; i++) {
+            if (i == 6 || i == 10) {
+                continue;
             }
-            if(i % 2 == 1) {
-                System.out.print("* ");
-            }
-            else {
+            if (i % 2 == 0) {
                 System.out.print(i + " ");
+            } else {
+                System.out.print("* ");
             }
         }
     }
