bf9fc33ffa12a0760dc5bfcbf5a98549401792c1
nanaputra <agenaputra@gmail.com>
2024-02-23 17:07:41 +0700

    Update buku01.java

--- week2/buku01.java
+++ week2/buku01.java
@@ -1,10 +1,10 @@
 package week2;
 
 public class buku01 {
-    String judul,pengarang;
+    String judul, pengarang;
     int halaman, stok, harga;
 
-    public buku01(){
+    public buku01() {
     }
 
     public buku01(String jud, String pg, int hal, int stok, int har) {
@@ -15,24 +15,48 @@ public class buku01 {
         harga = har;
     }
 
-    void tampilInformasi(){
+    void tampilInformasi() {
         System.out.println("Judul: " + judul);
         System.out.println("Pengarang: " + pengarang);
         System.out.println("Jumlah Halaman: " + halaman);
         System.out.println("Sisa stok: " + stok);
         System.out.println("Harga: Rp " + harga);
     }
+
     void terjual(int jml) {
         if (stok > 0) {
             stok -= jml;
         } else {
             System.out.println("Stok habis!");
         }
-    } 
+    }
+
     void restock(int jml) {
         stok += jml;
     }
+
     void gantiHarga(int hrg) {
         harga = hrg;
     }
+
+    int hitungHargaTotal(int jml) {
+        return harga * jml;
+    }
+
+    int hitungDiskon(int jml) {
+        int diskon = 0;
+        int hargaTotal = hitungHargaTotal(jml);
+        if(hargaTotal>=150000){
+            diskon = (int)(0.12*hargaTotal);
+        }else if(hargaTotal<150000 && hargaTotal>=75000)
+            diskon =(int)(0.05*hargaTotal);
+        else
+            diskon = 0;
+        return diskon;
+    }
+
+    int hitungHargaBayar(int jml) {
+        return hitungHargaTotal(jml) - hitungDiskon(jml);
+    }
 }
+
