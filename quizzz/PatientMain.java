4629d46f89142b8bd91e946c44cb311db0072f83
nanaputra <agenaputra@gmail.com>
2024-03-19 12:35:26 +0700

    Update PatientMain.java

--- quizzz/PatientMain.java
+++ quizzz/PatientMain.java
@@ -3,7 +3,7 @@ package quizzz;
 public class PatientMain {
 
     public static void main(String[] args) {
-        Patient pat1 = new Patient("p1", 15, 60000,30000);
+        
         
         double averageMedicationPrice; 
         double totalFee;
