6fa86cbc7b2b22a19984659549da3dcfd2bb64ce
nanaputra <agenaputra@gmail.com>
2024-03-31 20:46:58 +0700

    Update StudentList.java

--- week6/StudentList.java
+++ week6/StudentList.java
@@ -32,14 +32,34 @@ public class StudentList {
                 }
             }
         }
-    }/* */
-    /*//selection short method
+    }
+    //selection short method
     void selectionSort() {
         for(int i=0; i<list.length-1; i++) {
             int idxMin = i;
             for(int j=i+1; j<list.length; j++){
-                if(list[j].gpa.)
+                if(list[j].gpa < list[idxMin].gpa) {
+                    idxMin = j;
+        
+                }
             }
+            //swap
+            Student tmp = list [idxMin];
+            list [idxMin] = list [i];
+            list [i] = tmp;
         }
-    }/* */
+    }
+     // Insertion sort method
+     void insertionSort() {
+        for (int i = 1; i < idx; i++) {
+            Student key = list[i];
+            int j = i - 1;
+            while (j >= 0 && list[j].gpa > key.gpa) {
+                list[j + 1] = list[j];
+                j = j - 1;
+            }
+            list[j + 1] = key;
+        }
+    }
 }
+
