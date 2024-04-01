62a751ccf948ad5ed51a29203be867b98b37d2c3
nanaputra <agenaputra@gmail.com>
2024-04-01 19:17:36 +0700

    Update TicketService.java

--- week6/TicketService.java
+++ week6/TicketService.java
@@ -1,35 +1,38 @@
 package week6;
 
-import java.util.Arrays;
-
 public class TicketService {
     Ticket[] tickets;
     int size;
 
-    public TicketService() {
-        tickets = new Ticket[100];
+    public TicketService(int capacity) {
+        tickets = new Ticket[capacity];
         size = 0;
     }
 
-    public void add(Ticket t) {
-        tickets[size++] = t;
+    public void add(Ticket ticket) {
+        if (size < tickets.length) {
+            tickets[size++] = ticket;
+        } else {
+            System.out.println("Ticket service is full.");
+        }
     }
 
     public void displayAll() {
         for (int i = 0; i < size; i++) {
-            System.out.println(tickets[i]);
+            System.out.println("Ticket " + (i + 1) + ": " + tickets[i]);
         }
     }
 
-    // Bubble sort
-    public void bubbleSort() {
-        Arrays.sort(tickets, 0, size, (a, b) -> a.price - b.price);
-    }
-
-    // Selection sort
-    public void selectionSort() {
-        Arrays.sort(tickets, 0, size, (a, b) -> b.price - a.price);
+    public void bubbleSort(boolean ascending) {
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = 0; j < size - i - 1; j++) {
+                if ((ascending && tickets[j].price > tickets[j + 1].price)
+                        || (!ascending && tickets[j].price < tickets[j + 1].price)) {
+                    Ticket temp = tickets[j];
+                    tickets[j] = tickets[j + 1];
+                    tickets[j + 1] = temp;
+                }
+            }
+        }
     }
 }
-    
-
