bd3d395cfa35d88c69c2ce061c4c5452cd9d24f9
nanaputra <agenaputra@gmail.com>
2024-04-06 14:01:39 +0700

    Update SearchStudent.java

--- week7/SearchStudent.java
+++ week7/SearchStudent.java
@@ -3,6 +3,11 @@ package week7;
 public class SearchStudent {
     Students [] listStd = new Students[5];
     int idx;
+
+    public SearchStudent(int size) {
+        listStd = new Students[size];
+        idx = 0;
+    }
     
     public void add(Students std) {
         if(idx < listStd.length) {
@@ -20,33 +25,65 @@ public class SearchStudent {
             
         }
     }
-    public int findSeqSearch(int search) {
+    //Squential Search
+    public int findSeqSearch(String search) {
         int position = -1;
         for (int i = 0; i < listStd.length; i++) {
-            if(listStd[i].nim == search) {
-                position = 1;
+            if(listStd[i].name.equals(search)) {
+                position = i;
                 break;
             }
         }
         return position;
     }
-    public void showPosition(int x, int pos) {
-        if(pos != -1) {
-            System.out.println("Data : " + x + " is found in index-"+pos);
-        }else{
-            System.out.println("Data : " + x + " is not found");
+    //Binary search
+    public int findBinarySearch(int cari, int left, int right) {
+        sort();
+        int mid;
+        if (right >=left ) {
+            mid = (left + right) /2;
+            if (cari == listStd[mid].nim) {
+                return (mid);
+
+            }else if (listStd[mid].nim < cari) {
+                return findBinarySearch(cari, left, mid - 1);
+            } else {
+                return findBinarySearch(cari, mid + 1, right);
+            }
+        }
+        return -1;
+    }
+
+    private void sort() {
+        for (int i = 0; i < listStd.length - 1; i++) {
+            for (int j = 0; j < listStd.length - i - 1; j++) {
+                if (listStd[j].nim > listStd[j+1].nim) {
+                    Students temp = listStd[j];
+                    listStd[j] = listStd[j+1];
+                    listStd[j+1] = temp;
+                }
+            }
         }
     }
-    public void showData(int x, int pos) {
+    public void showPosition(String name, int pos) {
         if(pos != -1) {
-            System.out.println("NIM \t : " + x);
-            System.out.println("Name \t : " + listStd[pos].name);
+            System.out.println("Data : " + name + " is found in index-"+pos);
+        }else{
+            System.out.println("Data : " + name + " is not found");
+        }
+    }
+    public void showData(String name, int pos) {
+        if(pos != -1) {
+            System.out.println("Name \t : " + name);
+            System.out.println("NIM \t : " + listStd[pos].nim);
             System.out.println("Age \t : " + listStd[pos].age);
             System.out.println("IPK \t : " + listStd[pos].gpa);
         }else{
-            System.out.println("Data " + x + "is not found");
+            System.out.println("Data " + name + "is not found");
         }
     }
+
+    
 
 }
 
