0a86732d0ece1716f19a01dbb8a06a71e79aa519
nanaputra <agenaputra@gmail.com>
2024-04-06 14:01:25 +0700

    Create LargestValueFinder.java

---
+++ week7/LargestValueFinder.java
@@ -0,0 +1,16 @@
+package week7;
+
+public class LargestValueFinder {
+    public static int [] findLargestValue(int[] data) {
+        int largestPos = 0;
+        int largest = data[largestPos];
+        for (int i = 1; i < data.length; i++) {
+            if (data[i] > largest) {
+                largestPos = i;
+                largest = data[largestPos];
+            }
+        }
+        return new int[] {largestPos,largest};
+    }
+    
+}
