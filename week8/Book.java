122bd5a73800e0c0bb3a9bada15ef3888212366b
nanaputra <agenaputra@gmail.com>
2024-05-08 23:56:10 +0700

    Create Book.java

---
+++ week8/Book.java
@@ -0,0 +1,14 @@
+package week8;
+
+public class Book {
+    String title, authorName;
+    int publishedYear, pagesAmount, price;
+
+    public Book(String title, String authorName, int publishedYear, int pagesAmount, int price) {
+        this.title = title;
+        this.authorName = authorName;
+        this.publishedYear = publishedYear;
+        this.pagesAmount = pagesAmount;
+        this.price = price;
+    }
+}
