dc192932d1ebe9db45d5f47341be57af896809a7
nanaputra <agenaputra@gmail.com>
2024-05-17 12:39:40 +0700

    Create Passengers.java

---
+++ week9/Passengers.java
@@ -0,0 +1,17 @@
+package week9;
+
+public class Passengers {
+    String name;
+    String cityOrigin;
+    String cityDestination;
+    int ticketAmount;
+    int price;
+
+    public Passengers(String name, String cityOrigin, String cityDestination, int ticketAmount, int price) {
+        this.name = name;
+        this.cityOrigin = cityOrigin;
+        this.cityDestination = cityDestination;
+        this.ticketAmount = ticketAmount;
+        this.price = price;
+    }
+}
