71ba838e5e32898fbf86dcc1058239b270e80271
nanaputra <agenaputra@gmail.com>
2024-06-27 18:13:00 +0700

    Create Node.java

---
+++ week12/activities1/Node.java
@@ -0,0 +1,13 @@
+package week12.activities1;
+
+public class Node {
+    int data;
+    Node prev;
+    Node next;
+
+    public Node(Node prev, int data, Node next) {
+        this.data = data;
+        this.prev = prev;
+        this.next = next;
+    }
+}
