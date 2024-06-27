3effa537b94d6e91e28c85f4154652abd6271e62
nanaputra <agenaputra@gmail.com>
2024-06-27 19:29:07 +0700

    Create BinaryTreeArray.java

---
+++ week13/Assignment/BinaryTreeArray/BinaryTreeArray.java
@@ -0,0 +1,51 @@
+package week13.Assignment.BinaryTreeArray;
+
+public class BinaryTreeArray {
+    int[] data;
+    int idxLast;
+
+    public BinaryTreeArray(int size) {
+        data = new int[size];
+        idxLast = -1;
+    }
+
+    public void populateData(int value) {
+        if (idxLast < data.length - 1) {
+            data[++idxLast] = value;
+        }
+    }
+
+    public void traverseInOrder(int idxStart) {
+        if (idxStart <= idxLast) {
+            traverseInOrder(2 * idxStart + 1);
+            System.out.print(data[idxStart] + " ");
+            traverseInOrder(2 * idxStart + 2);
+        }
+    }
+
+    // Assignment: Add method to enter data into the tree
+    public void add(int value) {
+        if (idxLast < data.length - 1) {
+            data[++idxLast] = value;
+        }
+    }
+
+    // Assignment: Pre-Order Traversal
+    public void traversePreOrder(int idxStart) {
+        if (idxStart <= idxLast) {
+            System.out.print(data[idxStart] + " ");
+            traversePreOrder(2 * idxStart + 1);
+            traversePreOrder(2 * idxStart + 2);
+        }
+    }
+
+    // Assignment: Post-Order Traversal
+    public void traversePostOrder(int idxStart) {
+        if (idxStart <= idxLast) {
+            traversePostOrder(2 * idxStart + 1);
+            traversePostOrder(2 * idxStart + 2);
+            System.out.print(data[idxStart] + " ");
+        }
+    }
+}
+
