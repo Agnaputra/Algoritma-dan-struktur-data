61def2bca72ac502db26a96534daf76ba16d1fde
nanaputra <agenaputra@gmail.com>
2024-06-27 16:15:42 +0700

    Create Queue.java

---
+++ week11/Queue.java
@@ -0,0 +1,28 @@
+package week11;
+
+public class Queue {
+    private CustomerLinkedList list = new CustomerLinkedList();
+
+    public void enqueue(String name, String address, int accountNumber) {
+        list.addLast(name, address, accountNumber);
+    }
+
+    public void dequeue() {
+        list.removeFirst();
+    }
+
+    public void printQueue() {
+        list.print();
+    }
+
+    public static void main(String[] args) {
+        Queue queue = new Queue();
+        queue.enqueue("Alice", "123 Street A", 1001);
+        queue.enqueue("Bob", "456 Street B", 1002);
+        queue.enqueue("Charlie", "789 Street C", 1003);
+        queue.printQueue();
+
+        queue.dequeue();
+        queue.printQueue();
+    }
+}
