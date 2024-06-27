c31cc07ace32b1fd696fc5011e4bf83a6143fa08
nanaputra <agenaputra@gmail.com>
2024-06-27 19:29:14 +0700

    Create BinaryTree.java

---
+++ week13/Practikum1/BinaryTree.java
@@ -0,0 +1,116 @@
+package week13.Practikum1;
+
+public class BinaryTree {
+    Node root;
+
+    public BinaryTree() {
+        root = null;
+    }
+
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    public void add(int data) {
+        root = addRecursive(root, data);
+    }
+
+    private Node addRecursive(Node current, int data) {
+        if (current == null) {
+            return new Node(data);
+        }
+        if (data < current.data) {
+            current.left = addRecursive(current.left, data);
+        } else if (data > current.data) {
+            current.right = addRecursive(current.right, data);
+        }
+        return current;
+    }
+
+    public boolean find(int data) {
+        return findRecursive(root, data);
+    }
+
+    private boolean findRecursive(Node current, int data) {
+        if (current == null) {
+            return false;
+        }
+        if (data == current.data) {
+            return true;
+        }
+        return data < current.data ? findRecursive(current.left, data) : findRecursive(current.right, data);
+    }
+
+    public void traverseInOrder(Node node) {
+        if (node != null) {
+            traverseInOrder(node.left);
+            System.out.print(" " + node.data);
+            traverseInOrder(node.right);
+        }
+    }
+
+    public void traversePreOrder(Node node) {
+        if (node != null) {
+            System.out.print(" " + node.data);
+            traversePreOrder(node.left);
+            traversePreOrder(node.right);
+        }
+    }
+
+    public void traversePostOrder(Node node) {
+        if (node != null) {
+            traversePostOrder(node.left);
+            traversePostOrder(node.right);
+            System.out.print(" " + node.data);
+        }
+    }
+
+    public Node getSuccessor(Node delNode) {
+        Node successorParent = delNode;
+        Node successor = delNode;
+        Node current = delNode.right;
+
+        while (current != null) {
+            successorParent = successor;
+            successor = current;
+            current = current.left;
+        }
+
+        if (successor != delNode.right) {
+            successorParent.left = successor.right;
+            successor.right = delNode.right;
+        }
+        return successor;
+    }
+
+    public void delete(int data) {
+        root = deleteRecursive(root, data);
+    }
+
+    private Node deleteRecursive(Node current, int data) {
+        if (current == null) {
+            return null;
+        }
+        if (data == current.data) {
+            if (current.left == null && current.right == null) {
+                return null;
+            }
+            if (current.right == null) {
+                return current.left;
+            }
+            if (current.left == null) {
+                return current.right;
+            }
+            Node successor = getSuccessor(current);
+            current.data = successor.data;
+            current.right = deleteRecursive(current.right, successor.data);
+            return current;
+        }
+        if (data < current.data) {
+            current.left = deleteRecursive(current.left, data);
+            return current;
+        }
+        current.right = deleteRecursive(current.right, data);
+        return current;
+    }
+}
