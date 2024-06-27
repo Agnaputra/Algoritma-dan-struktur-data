d5c6c055a367ca1009d371ae4b136f4ce849907a
nanaputra <agenaputra@gmail.com>
2024-06-27 16:16:08 +0700

    Create StackMain.java

---
+++ week11/assignment/no3/StackMain.java
@@ -0,0 +1,22 @@
+package week11.assignment.no3;
+
+public class StackMain {
+    public static void main(String[] args) {
+        // Create the stack
+        Stack stack = new Stack();
+
+        // Push elements onto the stack
+        stack.push("Multimedia");
+        stack.push("Statistika");
+        stack.push("Algoritma");
+        stack.push("Matematika");
+        stack.push("Basis Data");
+        stack.push("Komputer");
+        stack.push("Android");
+        stack.push("Bahasa");
+
+        // Display the stack
+        stack.display();
+    }
+}
+
