fd9258c5ff494a407ddefc909f725b9a91b69811
nanaputra <agenaputra@gmail.com>
2024-08-30 04:06:22 +0700

    Update Graph.java

--- week14/Assignment/Graph.java
+++ week14/Assignment/Graph.java
@@ -1,37 +1,43 @@
-package week14.Assignment;
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Graph<T> {
-    private LinkedList<T>[] adjLists;
+    private Map<T, LinkedList<T>> adjLists;
     private boolean isDirected;
 
-    public Graph(int vertices, boolean isDirected) {
+    public Graph(boolean isDirected) {
         this.isDirected = isDirected;
-        adjLists = new LinkedList[vertices];
-        for (int i = 0; i < vertices; i++) {
-            adjLists[i] = new LinkedList<>();
-        }
+        adjLists = new HashMap<>();
+    }
+
+    public void addVertex(T vertex) {
+        adjLists.putIfAbsent(vertex, new LinkedList<>());
     }
 
     public void addEdge(T src, T dest) {
-        adjLists[(int) src].add(dest);
+        adjLists.get(src).add(dest);
         if (!isDirected) {
-            adjLists[(int) dest].add(src);
+            adjLists.get(dest).add(src);
         }
     }
 
     public void removeEdge(T src, T dest) {
-        adjLists[(int) src].remove(dest);
-        if (!isDirected) {
-            adjLists[(int) dest].remove(src);
+        LinkedList<T> srcList = adjLists.get(src);
+        LinkedList<T> destList = adjLists.get(dest);
+        if (srcList != null) {
+            srcList.remove(dest);
+        }
+        if (!isDirected && destList != null) {
+            destList.remove(src);
         }
     }
 
     public void printGraph() {
-        for (int i = 0; i < adjLists.length; i++) {
-            System.out.print(i + " is connected to: ");
-            for (T vertex : adjLists[i]) {
+        for (Map.Entry<T, LinkedList<T>> entry : adjLists.entrySet()) {
+            System.out.print(entry.getKey() + " is connected to: ");
+            for (T vertex : entry.getValue()) {
                 System.out.print(vertex + " ");
             }
             System.out.println();
@@ -40,25 +46,28 @@ public class Graph<T> {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Graph<String> graph = new Graph<>(false); // or true for directed graph
+
         System.out.println("Enter number of vertices: ");
         int vertices = scanner.nextInt();
+        scanner.nextLine(); // consume newline
 
-        System.out.println("Is the graph directed? (true/false): ");
-        boolean isDirected = scanner.nextBoolean();
-
-        Graph<Integer> graph = new Graph<>(vertices, isDirected);
+        System.out.println("Enter vertices: ");
+        for (int i = 0; i < vertices; i++) {
+            String vertex = scanner.nextLine();
+            graph.addVertex(vertex);
+        }
 
         System.out.println("Enter number of edges: ");
         int edges = scanner.nextInt();
+        scanner.nextLine(); // consume newline
 
         System.out.println("Enter edges (format: src dest): ");
         for (int i = 0; i < edges; i++) {
-            int src = scanner.nextInt();
-            int dest = scanner.nextInt();
+            String src = scanner.next();
+            String dest = scanner.next();
             graph.addEdge(src, dest);
         }
-
-        graph.printGraph();
+         graph.printGraph();
     }
 }
-
